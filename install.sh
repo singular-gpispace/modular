d4416f9f2ac58d9a868eac9ece48b4a86a8fe146
hobihasina <hobihasina@aims.ac.za>
2022-06-30 14:38:56 +0200

    mod_input_data

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 rm -r framework_build
-cmake -D CMAKE_INSTALL_PREFIX="../framework-mod2_install" \
+cmake -D CMAKE_INSTALL_PREFIX="../framework-mod_install" \
       -D CMAKE_BUILD_TYPE=Release                       \
       -D SINGULAR_HOME=/opt/Singular430/Singular      \
       -B framework_build                                \
