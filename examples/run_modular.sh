24b93372eacc0643d6365d226c426c662360c2a2
hobihasina <hobihasina@aims.ac.za>
2023-05-03 00:24:26 +0200

    clean up

--- examples/run_modular.sh
+++
@@ -1,5 +0,0 @@
-cd temp
-rm * 
-cd ..
-gspc-monitor --port 9876 &
-SINGULARPATH=/home/hbn/Documents/AIMS/projet/master/github/framework_independent_sub_install5 Singular run_modular.sing
--- install.sh
+++
@@ -1,8 +0,0 @@
-#rm -r framework_build
-cmake -D CMAKE_INSTALL_PREFIX="../framework_independent_sub_install5" \
-      -D CMAKE_BUILD_TYPE=Release                       \
-      -D SINGULAR_HOME=/home/hbn/Singular4/Singular      \
-      -B framework_build                                \
-      -S .
-cmake --build framework_build --target install -j $(nproc)
-
