ba9e1c7f887bb3276a8a1ee7d170784998a912f9
hobihasina <hobihasina@aims.ac.za>
2023-07-23 15:56:45 +0200

    add more std examples

---
+++ install.sh
@@ -0,0 +1,9 @@
+#rm -r framework_build
+cmake -D CMAKE_INSTALL_PREFIX="../framework_install" \
+      -D CMAKE_BUILD_TYPE=Release                       \
+      -D SINGULAR_HOME="/home/hbn/spack/opt/spack/linux-ubuntu20.04-icelake/gcc-9.4.0/singular-snapshot_22_03-qqvuclfdgojic7xdjgcgw422b6ao4p7y"      \
+      -D Boost_NO_BOOST_CMAKE=on  \
+      -B framework_build                                \
+      -S .
+cmake --build framework_build --target install -j $(nproc)
+
