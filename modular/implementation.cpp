940f711fd9cc4d233e780961c0a7267850809562
hobihasina <hobihasina@aims.ac.za>
2023-12-07 22:02:35 +0300

    reference in function split

--- modular/implementation.cpp
+++ modular/implementation.cpp
@@ -97,30 +97,11 @@ std::string  singular_modular_lift( std::string const& left
 }
 
 
-/*std::string singular_modular_farey (std::string const& lifted_res
-                                   , std::string const& function_name
-                                   , std::string const& needed_library
-                                   , std::string const& base_filename
-                                   )
-{
-  init_singular (config::library().string());
-  load_singular_library(needed_library);
-  std::pair<int, lists> liftedres;
-  std::pair<int, lists> out;
-  std::string ids;
-  std::string out_filename;
-  ids = worker();
-  liftedres = deserialize(lifted_res,ids);
-  ScopedLeftv args( liftedres.first, lCopy(liftedres.second));
-  out = call_user_proc(function_name, needed_library, args);
-  out_filename = serialize(out.second, base_filename);
-  return out_filename;
-}*/
 
-std::pair<std::vector<std::string>, unsigned long> singular_modular_split(std::string const& in,
-                                                std::string const& function_name,
-                                                std::string const& needed_library,
-                                                std::string const& base_filename)
+unsigned long singular_modular_split(std::string const& in,
+                                      std::string const& function_name,
+                                      std::string const& needed_library
+                                      )
 {
   init_singular (config::library().string());
   load_singular_library(needed_library);
@@ -134,18 +115,15 @@ std::pair<std::vector<std::string>, unsigned long> singular_modular_split(std::s
   ScopedLeftv args( input.first, lCopy(input.second));
   out = call_user_proc(function_name, needed_library, args);
   lists u = (lists)out.second->m[3].Data();//ring-lists-ring-lists
-  for(int i (0); i<lSize(u); i++)
-  {
-    vec.push_back(serialize((lists)u->m[i].Data(), base_filename));
-  } 
-  total_generator = (unsigned long) (int) (long) u->m[lSize(u)].Data();
-  return {vec, total_generator};
+  total_generator = (unsigned long) (int) (long) u->m[0].Data();
+  return total_generator;
 }
 
-std::pair<std::string, unsigned long> singular_modular_farey(std::string const& in,
-                                                             std::string const& function_name,
-                                                             std::string const& needed_library,
-                                                             std::string const& base_filename)
+std::string singular_modular_farey(std::string const& in, 
+                                   unsigned long const& gen_number,
+                                   std::string const& function_name,
+                                   std::string const& needed_library,
+                                   std::string const& base_filename)
 {
   init_singular (config::library().string());
   load_singular_library(needed_library);
@@ -153,14 +131,14 @@ std::pair<std::string, unsigned long> singular_modular_farey(std::string const& 
   std::pair<int, lists> out;
   std::string out_filename;
   std::string ids;
-  unsigned long N;
+  void* N =  (char*) (long) (gen_number);
 	ids = worker();
   input = deserialize(in,ids);
-  out = call_user_proc(function_name, needed_library, input.first, input.second);
-  lists u = (lists) out.second->m[3].Data();
-  N =(unsigned long) (int) (long) u->m[1].Data();
+  ScopedLeftv args(input.first, lCopy(input.second));
+	ScopedLeftv arg(args,INT_CMD,N);
+  out = call_user_proc(function_name, needed_library, args);
   out_filename = serialize(out.second, base_filename);
-  return {out_filename,N};
+  return out_filename;
 }
 
 std::string  singular_modular_append(std::string const& left
@@ -189,6 +167,7 @@ std::string  singular_modular_append(std::string const& left
 
 bool singular_modular_compatible (std::string const& result_farey
                                    , std::string const& test_result
+                                   , unsigned long const& gen_number
                                    , std::string const& function_name
                                    , std::string const& needed_library
                                    )
@@ -200,11 +179,13 @@ bool singular_modular_compatible (std::string const& result_farey
   std::pair<int, lists> out;
   std::string ids;
   bool testt;
+  void* N = (char*) (long) gen_number; 
   ids = worker();
   resultfarey = deserialize(result_farey,ids);
   testresult = deserialize(test_result, ids);
   ScopedLeftv args( resultfarey.first, lCopy(resultfarey.second));
   ScopedLeftv arg(args,testresult.first,lCopy(testresult.second));
+  ScopedLeftv ar(args,INT_CMD,N);
   out = call_user_proc(function_name, needed_library, args);
   lists u = (lists) out.second->m[3].Data();
   testt = (int) (long) u->m[0].Data();
--- share/include/modular_interface.hpp
+++ share/include/modular_interface.hpp
@@ -33,25 +33,19 @@ std::string  singular_modular_lift( std::string const&
                                       , std::string const& 
                                       );
 NO_NAME_MANGLING
-std::pair<std::vector<std::string>, unsigned long> singular_modular_split(std::string const&
-                                                , std::string const&
-                                                , std::string const& 
-                                                , std::string const&
-                                                );
+unsigned long singular_modular_split(std::string const&
+                                    , std::string const&
+                                    , std::string const& 
+                                    );
 
-/*NO_NAME_MANGLING
-std::string singular_modular_farey (std::string const&
-                                   , std::string const&
-                                   , std::string const&
-                                   , std::string const&
-                                   );*/
 
 NO_NAME_MANGLING
-std::pair<std::string, unsigned long> singular_modular_farey(std::string const& 
-                                                             , std::string const& 
-                                                             , std::string const&
-                                                             , std::string const&
-                                                             );
+std::string singular_modular_farey(std::string const& 
+                                 , unsigned long const&
+                                 , std::string const& 
+                                 , std::string const&
+                                , std::string const&     
+                                );
 
 NO_NAME_MANGLING
 std::string  singular_modular_append(std::string const& 
@@ -64,6 +58,7 @@ std::string  singular_modular_append(std::string const&
 NO_NAME_MANGLING
 bool singular_modular_compatible (std::string const& 
                                    , std::string const&
+                                   , unsigned long const&
                                    , std::string const& 
                                    , std::string const& 
                                    );
